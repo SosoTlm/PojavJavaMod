403b29829cb2dfa7ad594a43cfdac94f24fbcf36
SosoTlm <nitogx21@gmail.com>
2024-08-01 12:21:52 +0200

    update Main/src/main/Java/main.java

---
+++ Main/src/main/Java/main.java
@@ -0,0 +1,10 @@
+package com.example.cachecache;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CacheCacheMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Cache-cache mod initialized");
+    }
+}
